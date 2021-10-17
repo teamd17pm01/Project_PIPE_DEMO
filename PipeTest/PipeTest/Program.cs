12a1e6d884a7883514b2a0b319c20cdd8d92602c
phhung <phamhoaihung2408@gmail.com>
2021-10-18 01:43:27 +0700

    First commit

---
+++ PipeTest/PipeTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace PipeTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("------------CLIENT--------------");
+            var client = new NamedPipeClientStream("PipesOfPices");
+            Console.WriteLine("Client waiting connect.....");
+            client.Connect();
+            Console.WriteLine("Client connected to server.....!");
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+            while (true)
+            {
+                Console.WriteLine("Client want: ");
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+                writer.WriteLine(input);
+                writer.Flush();
+                Console.WriteLine("Server: " + reader.ReadLine());
+            }
+        }
+    }
+}
---
+++ ServerPipes/ServerPipes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Pipes;
+using System.IO;
+
+namespace ServerPipes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------SERVER----------------");
+            var server = new NamedPipeServerStream("PipesOfPices");
+            Console.WriteLine("Server waiting connect....");
+            server.WaitForConnection();
+            Console.WriteLine("Server connected to client!");
+
+            //Đọc hoặc nhận thông điệp từ server
+            StreamReader reader = new StreamReader(server);
+            StreamWriter writer = new StreamWriter(server);
+            while (true)
+            {
+                //Nhận thông điệp từ client
+                Console.WriteLine("Client:" + reader.ReadLine());
+                Console.Write("Server want: ");
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+                writer.WriteLine(input);
+                writer.Flush();
+            }
+        }
+    }
+}
